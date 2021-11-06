5c814d18ec300d8a9a5baf82a39c5126647bb673
Aloysious Benoy <aloysiousbenoy@gmail.com>
2021-11-06 22:48:39 +0530

    Progress 18 %

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,9 +1,9 @@
 // variables4.rs
 // Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x: i32;
+    let x: i32 = 34;
+    let y: i32 = 35;
+    println!("{}", y + x);
     println!("Number {}", x);
 }
